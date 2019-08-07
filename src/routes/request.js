4906bc4d52f6a6b0ea09033623234915dfc412ee
Asmaathabet <asmathabet200@hotmail.com>
2019-08-07 16:00:53 +0300

    style

--- src/routes/request.js
+++ src/routes/request.js
@@ -4,16 +4,17 @@ require('dotenv').config();
 const apiKey = process.env.API_KEY;
 
 exports.fetchData = (req, res) => {
-  // console.log(req.body);
-  const search = req.body.query;
-  const url = `https://newsapi.org/v2/everything?q=${search}&apiKey=${apiKey}`;
+    // console.log(req.body);
+    const search = req.body.query;
+    const url = `https://newsapi.org/v2/everything?q=${search}&apiKey=${apiKey}`;
 
-  fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      res.render('home', {
-        result: data.articles,
-      });
-    })
-    .catch(err => console.log(err));
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            res.render('home', {
+                title: 'Know More',
+                result: data.articles,
+            });
+        })
+        .catch(err => console.log(err));
 };
