128534d3915bc5688a15bceff75c86af5619c2e1
mossa-Sammer <mossa708496@gmail.com>
2019-08-07 15:31:27 +0300

    make tests and handle errors relates #14

--- test/news.test.js
+++ test/news.test.js
@@ -1,6 +1,34 @@
 const test = require('tape');
 
+const supertest = require('supertest');
+
+const app = require('../src/app');
+
 test('inital test', (t) => {
   t.equal(2, 2, 'should be 2');
   t.end();
+});
+
+test('test success search', (t) => {
+  supertest(app)
+    .get('/')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err, 'should not return err');
+      const isInclude = res.text.includes('Know More');
+      t.equals(isInclude, true, 'the bage should be Know More');
+      t.end();
+    });
+});
+
+test('test for search', (t) => {
+  supertest(app)
+    .post('/search')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err) => {
+      t.error(err, 'error with headers');
+      t.end();
+    });
 });
